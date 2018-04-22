c1ef569f09cd5dc33ea9557b25dc263c6af830a6
Jari Kuisma <kuismaj@Jari-MacBook-Air.local>
2018-04-22 11:17:57 +0300

    Add template environment files. In order to build the project and get it running those should be supplied with own data. Also the *.temple* from the file name needs to be removed.

---
+++ src/environments/environment.prod.template.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "{{ YOUR OWN VALUES }}",
+    authDomain: "{{ YOUR OWN VALUES }}",
+    databaseURL: "{{ YOUR OWN VALUES }}",
+    projectId: "{{ YOUR OWN VALUES }}",
+    storageBucket: "{{ YOUR OWN VALUES }}",
+    messagingSenderId: "{{ YOUR OWN VALUES }}"
+  }
+};
