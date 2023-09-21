cb31bc18a9038c6c53361a2187846692eea6e587
Jari Kuisma <jari.kuisma@gmail.com>
2023-09-21 20:45:40 +0300

    Update favicon. Small style fixes.

--- src/app/transition-group-item.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TransitionGroupItemDirective } from './transition-group-item.directive';
-
-describe('TransitionGroupItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TransitionGroupItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
