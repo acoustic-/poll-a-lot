2ccd32aed68c7f91a875bd30c9fb7306b6e96e32
Jari Kuisma <jari.kuisma@gmail.com>
2024-01-28 14:39:50 +0200

    Improve footer formatting and content.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,16 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+  selector: "footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent implements OnInit {
   window = window;
-  
-  constructor() { }
+  today = Date.now();
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
