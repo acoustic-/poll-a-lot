f71fde8d354abde6efbc469310c2e62f7179d0e8
Jari Kuisma <jari.kuisma@gmail.com>
2024-02-28 11:01:27 +0200

    Improve update logic to happen on init automatically and then poll optional update every 60s.

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -3,22 +3,43 @@ import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { SwUpdate } from "@angular/service-worker";
 import { from, timer } from "rxjs";
-import { filter } from "rxjs/operators";
+import { filter, map, switchMap } from "rxjs/operators";
 
 @Injectable()
 export class UpdateService {
   constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
-    from(this.swUpdate.checkForUpdate())
-      .pipe(filter((update) => !!update))
-      .subscribe(() => {
-        const snack = this.snackbar.open("Update Available", "Reload");
+    // Force update on init
+    setTimeout(() =>
+      timer(2000)
+        .pipe(
+          switchMap(() => from(this.swUpdate.checkForUpdate())),
+          filter((update) => !!update)
+        )
+        .subscribe(async () => await this.update())
+    );
 
-        snack.onAction().subscribe(() => {
-          window.location.reload();
+    // Give option for following update requests
+    timer(60000, 60000)
+      .pipe(
+        switchMap(() => from(this.swUpdate.checkForUpdate())),
+        filter((update) => !!update)
+      )
+      .subscribe(async () => {
+        const snack = this.snackbar.open("Update Available", "Reload", {
+          duration: 10000,
         });
-
+        snack.onAction().subscribe(async () => {
+          await this.update();
+        });
       });
+  }
 
-    timer(1000 * 60 * 2).subscribe(() => swUpdate.checkForUpdate());
+  async update() {
+    window.location.reload();
+    const snack = this.snackbar.open(
+      "Poll-a-Lot was updated to latest version! ✨",
+      undefined,
+      { duration: 5000 }
+    );
   }
 }
